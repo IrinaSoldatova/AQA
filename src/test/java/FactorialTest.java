619c139f66fbcee37e023f3fb431f63af2545b41
Irina Soldatova <soldatovaik@yandex.ru>
2025-01-21 23:04:37 +0300

    add lesson 14_junit_5

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,38 +1,37 @@
 import org.example.lesson_14.Factorial;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-import static org.testng.Assert.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FactorialTest {
 
-    @DataProvider(name = "factorialData")
-    public static Object[][] factorialData() {
-        return new Object[][] {
-                {0, 1L},
-                {1, 1L},
-                {2, 2L},
-                {3, 6L},
-                {4, 24L},
-                {5, 120L},
-                {10, 3628800L},
-                {20, 2432902008176640000L}
-        };
-    }
-
-    @Test(dataProvider = "factorialData")
-    public void testCalculateFactorialWithValidInput(int n, long expected) {
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1",
+            "1, 1",
+            "2, 2",
+            "3, 6",
+            "4, 24",
+            "5, 120",
+            "10, 3628800",
+            "20, 2432902008176640000"
+    })
+    void testCalculateFactorialWithValidInput(int n, long expected) {
         long result = Factorial.calculateFactorial(n);
-        assertEquals(result, expected, "Факториал от " + n + " должен быть равен " + expected);
-    }
-
-    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Факториал не определен для отрицательных чисел.")
-    public void testCalculateFactorialWithNegativeInput() {
-        Factorial.calculateFactorial(-5);
+        assertEquals(expected, result, "Факториал от " + n + " должен быть равен " + expected);
     }
 
 
-    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Факториал слишком большого числа.")
-    public void testCalculateFactorialWithTooLargeInput() {
-        Factorial.calculateFactorial(21);
+    @Test
+    void testCalculateFactorialWithNegativeInput() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> Factorial.calculateFactorial(-5));
+        assertEquals("Факториал не определен для отрицательных чисел.",thrown.getMessage());
+    }
+
+    @Test
+    void testCalculateFactorialWithTooLargeInput() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> Factorial.calculateFactorial(21));
+        assertEquals("Факториал слишком большого числа.",thrown.getMessage());
     }
 }

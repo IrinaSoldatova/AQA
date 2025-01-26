30ea8ed109e28768eda0b6e071a68b9bcf05f7c4
Irina Soldatova <soldatovaik@yandex.ru>
2025-01-26 23:22:08 +0300

    add lesson_15

---
+++ src/test/java/MtsTests.java
@@ -0,0 +1,144 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+
+public class MtsTests {
+
+    private WebDriver driver;
+    private static final String MTS_URL = "https://www.mts.by/";
+
+    //Проверка Pop-up c cookie
+    @BeforeClass
+    public void setupDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(MTS_URL);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        try {
+            WebElement acceptCookiesButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='cookie-agree']")));
+            acceptCookiesButton.click();
+        } catch (TimeoutException e) {
+            System.out.println("Pop-up c cookie не обнаружен");
+        }
+
+
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(webDriver -> ((JavascriptExecutor) webDriver)
+                        .executeScript("return document.readyState")
+                        .equals("complete"));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    // Проверка наличия и названия блока "Онлайн пополнение без комиссии"
+    @Test
+    public void testBlockTitle() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement block = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div")));
+        Assert.assertTrue(block.isDisplayed(), "Блок \"Онлайн пополнение без комиссии\" не отображается");
+
+        WebElement blockTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/h2")));
+        Assert.assertEquals(blockTitle.getText().replace("\n", " "), "Онлайн пополнение без комиссии", "Название блока не соответствует");
+    }
+
+    // Проверка логотипов платежных систем
+    @Test
+    public void testPaymentSystemLogos() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement logosContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[2]/ul")));
+        Assert.assertTrue(logosContainer.isDisplayed(), "Логотипы платежных систем отсутсвуют.");
+
+        String[] expectedLogos = {
+                "Visa",
+                "Verified By Visa",
+                "MasterCard",
+                "MasterCard Secure Code",
+                "Белкарт"
+        };
+
+        String[] expectedXPaths = {
+                "//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[2]/ul/li[1]/img",
+                "//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[2]/ul/li[2]/img",
+                "//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[2]/ul/li[3]/img",
+                "//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[2]/ul/li[4]/img",
+                "//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[2]/ul/li[5]/img"
+        };
+
+        for (int i = 0; i < expectedLogos.length; i++) {
+            WebElement logo = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(expectedXPaths[i])));
+            Assert.assertTrue(logo.isDisplayed(), "Логотип " + expectedLogos[i] + " не отображается");
+            Assert.assertTrue(logo.getAttribute("alt").contains(expectedLogos[i]),"Неверный alt у лого " + expectedLogos[i]);
+        }
+    }
+
+    // Проверка ссылки "Подробнее о сервисе"
+    @Test
+    public void testServiceDetailsLink() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        WebElement detailsLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/a")));
+        Assert.assertTrue(detailsLink.isDisplayed(), "Ссылка 'Подробнее о сервисе' не найдена.");
+
+        String originalUrl = driver.getCurrentUrl();
+        detailsLink.click();
+
+        try {
+            wait.until(ExpectedConditions.urlToBe("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/"));
+        } catch (TimeoutException e) {
+            System.out.println("URL не загружен в течение 20 секунд: " + e.getMessage());
+            Assert.fail("Не удалось загрузить ожидаемый URL.");
+        }
+
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertNotEquals(currentUrl, originalUrl, "Ссылка 'Подробнее о сервисе' не перенаправляет на другую страницу.");
+    }
+
+    // Проверка полей ввода и кнопки "Продолжить"
+    @Test
+    public void testContinueButtonForServices() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        WebElement serviceButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[1]/div[1]/div[2]/button")));
+        serviceButton.click();
+
+        WebElement serviceOption = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[1]/div[1]/div[2]/button/span[1]")));
+        serviceOption.click();
+
+        WebElement phoneNumberField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"connection-phone\"]")));
+        phoneNumberField.sendKeys("297777777");
+
+        WebElement amountField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"connection-sum\"]")));
+        amountField.sendKeys("2");
+
+        WebElement continueButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"pay-connection\"]/button")));
+        continueButton.click();
+
+        WebElement paymentIframe = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//iframe[contains(@class, 'bepaid-iframe')]")));
+
+        driver.switchTo().frame(paymentIframe);
+
+        WebElement closeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/app-root/div/div/app-header/header/div/app-back-navigation/div/div")));
+        closeButton.click();
+
+        driver.switchTo().defaultContent();
+    }
+}
